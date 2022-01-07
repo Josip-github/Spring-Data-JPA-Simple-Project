f5d0758c5078c3e09bd4ac9cc73941f6ea2a9f50
Josip-github <josipcota1@gmail.com>
2022-01-07 12:15:06 +0100

    This commit creates a test method for creating a patient instance. The test passes and there are now two rows executed by Hibernate in the database: insert into patient and insert into patients_doctors beacuse I've set the doctor for the patient.

--- src/test/java/com/bharath/springdata/patientscheduling/PatientSchedulingApplicationTests.java
+++ src/test/java/com/bharath/springdata/patientscheduling/PatientSchedulingApplicationTests.java
@@ -1,16 +1,26 @@
 package com.bharath.springdata.patientscheduling;
 
 import com.bharath.springdata.patientscheduling.model.Doctor;
+import com.bharath.springdata.patientscheduling.model.Insurance;
+import com.bharath.springdata.patientscheduling.model.Patient;
 import com.bharath.springdata.patientscheduling.repository.DoctorRepository;
+import com.bharath.springdata.patientscheduling.repository.PatientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class PatientSchedulingApplicationTests {
 
     @Autowired
     DoctorRepository doctorRepository;
+
+    @Autowired
+    PatientRepository patientRepository;
 
     @Test
     void contextLoads() {
@@ -24,6 +34,28 @@ class PatientSchedulingApplicationTests {
         doctor.setSpeciality("All");
 
         doctorRepository.save(doctor);
+    }
+
+    @Test
+    public void testCreatePatient(){
+        Patient patient = new Patient();
+        patient.setFirstName("Julia");
+        patient.setLastName("Roberts");
+        patient.setPhone("123456");
+
+        Insurance insurance = new Insurance();
+
+        insurance.setProviderName("Blue Cross Blue Shield");
+        insurance.setCopay(20.0);
+
+        patient.setInsurance(insurance);
+
+        Optional<Doctor> doctor = doctorRepository.findById(1l);
+        List<Doctor> doctors = Arrays.asList(doctor.get());
+
+        patient.setDoctors(doctors);
+
+        patientRepository.save(patient);
     }
 
 }
