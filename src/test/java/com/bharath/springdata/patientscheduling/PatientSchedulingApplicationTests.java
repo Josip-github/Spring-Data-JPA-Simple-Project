cc0372d1912170b7b753905cf043b7bc2f777922
Josip-github <josipcota1@gmail.com>
2022-01-07 12:04:53 +0100

    This commit creates a test method for creating a doctor instance. The tests passes and there is one row in the database.

--- src/test/java/com/bharath/springdata/patientscheduling/PatientSchedulingApplicationTests.java
+++ src/test/java/com/bharath/springdata/patientscheduling/PatientSchedulingApplicationTests.java
@@ -1,13 +1,29 @@
 package com.bharath.springdata.patientscheduling;
 
+import com.bharath.springdata.patientscheduling.model.Doctor;
+import com.bharath.springdata.patientscheduling.repository.DoctorRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PatientSchedulingApplicationTests {
 
+    @Autowired
+    DoctorRepository doctorRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testCreateDoctor(){
+        Doctor doctor = new Doctor();
+        doctor.setFirstName("John");
+        doctor.setLastName("Brown");
+        doctor.setSpeciality("All");
+
+        doctorRepository.save(doctor);
     }
 
 }
